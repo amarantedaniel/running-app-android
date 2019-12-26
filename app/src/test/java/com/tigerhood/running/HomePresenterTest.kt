2b3c2472953cd93665c18d4c0fa2e349ccb7c234
Daniel Amarante <daniel.amarante2@gmail.com>
2019-12-26 15:00:54 -0300

    Refactor tests

--- app/src/test/java/com/tigerhood/running/HomePresenterTest.kt
+++ app/src/test/java/com/tigerhood/running/HomePresenterTest.kt
@@ -2,18 +2,32 @@ package com.tigerhood.running
 
 import com.tigerhood.running.entity.WorkoutDay
 import com.tigerhood.running.presenter.HomePresenter
+import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.*
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
 
-
+@RunWith(MockitoJUnitRunner::class)
 class HomePresenterTest {
-    @Test
-    fun whenOnViewCreatedIsCalledShouldWork() {
-        val interactor = mock(HomeContract.Interactor::class.java)
-        val view = mock(HomeContract.View::class.java)
-        val day = WorkoutDay("1", 1, 1, "Description 1", listOf())
-        `when`(interactor.loadWorkoutDays()).thenReturn(listOf(day))
 
+    @Mock
+    private lateinit var view: HomeContract.View
+
+    @Mock
+    private lateinit var interactor: HomeContract.Interactor
+
+    private val day = WorkoutDay("1", 1, 1, "Description 1", listOf())
+
+    @Before
+    fun init() {
+        `when`(interactor.loadWorkoutDays()).thenReturn(listOf(day))
+    }
+
+    @Test
+    fun onViewCreated_shouldCallViewFunctionsWithListOfDays() {
         val presenter = HomePresenter(view, interactor)
         presenter.onViewCreated()
 
