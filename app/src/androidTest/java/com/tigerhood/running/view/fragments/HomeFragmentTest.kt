d9e775d876cdcde042cdfd06cd165febbddcdcd1
Daniel Amarante <daniel.amarante2@gmail.com>
2019-12-30 11:47:16 -0300

    Add test for recycler view loading and clicking

--- app/src/androidTest/java/com/tigerhood/running/view/fragments/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/tigerhood/running/view/fragments/HomeFragmentTest.kt
@@ -3,12 +3,15 @@ package com.tigerhood.running.view.fragments
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.tigerhood.running.HomeContract
 import com.tigerhood.running.R
+import com.tigerhood.running.entity.WorkoutDay
 import com.tigerhood.running.factory.HomeFactory
 import org.junit.Before
 import org.junit.Test
@@ -20,6 +23,7 @@ class HomeFragmentTest {
 
     private lateinit var scenario: FragmentScenario<HomeFragment>
     private val presenter = mock(HomeContract.Presenter::class.java)
+    private val day = WorkoutDay("1", 1, 1, "Description 1", listOf())
 
     @Before
     fun setUp() {
@@ -48,6 +52,17 @@ class HomeFragmentTest {
     fun onDestroyView_shouldCallPresenterOnDestroy() {
         scenario.moveToState(Lifecycle.State.DESTROYED)
         verify(presenter).onDestroy()
+    }
 
+    @Test
+    fun shouldCallPresenterOnSelectWhenDayIsSelected() {
+        scenario.onFragment { it.updateWorkoutDays(listOf(day)) }
+        onView(withId(R.id.recyclerView)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        verify(presenter).onDaySelected(day)
     }
 }
